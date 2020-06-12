7546cb5760bca782c36ee0736c2ef4c944039f96
KhanhLDGCS18304 <66111389+KhanhLDGCS18304@users.noreply.github.com>
2020-06-12 12:03:09 +0700

    Update Test2.js

--- Test2.js
+++ Test2.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var lanthu=0;
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 function Xulyweb (req, res) {
     lanthu++;
   res.writeHead(200, {'Content-Type': 'text/html'});
